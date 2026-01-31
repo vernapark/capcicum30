6500ef20cc1b2cb977b29448e0673cf8836434ce
vernapark <aryanbitxx3@gmail.com>
2026-01-31 06:55:22 +0000

    Modified admin panel and server js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 let messages = [];
+let devices = [];
 
 app.post('/receive-sms', (req, res) => {
     const { sender, message } = req.body;
@@ -26,6 +27,16 @@ app.post('/send-sms', (req, res) => {
     const { recipient, message, simSlot } = req.body;
     // Here you would add the logic to send the SMS using the specified simSlot
     res.sendStatus(200);
+});
+
+app.post('/register-device', (req, res) => {
+    const { deviceId, ipAddress, phoneNumber1, phoneNumber2 } = req.body;
+    devices.push({ deviceId, ipAddress, phoneNumber1, phoneNumber2 });
+    res.sendStatus(200);
+});
+
+app.get('/devices', (req, res) => {
+    res.json(devices);
 });
 
 app.listen(PORT, () => {
